a0ffea8a471c00d12fd5a3c96f3c4e3aec49871b
rashidulislamg71 <jrashidulislam0103@gmail.com>
2024-09-28 09:29:55 +0600

    navBar design

--- src/Component/MainLayout/Header/NavBar.jsx
+++ src/Component/MainLayout/Header/NavBar.jsx
@@ -1,97 +1,101 @@
-// import React from 'react';
+import React, { useRef, useState } from "react";
 
-// import { NavLink } from 'react-router-dom';
-// import { PiShoppingCartThin } from "react-icons/pi";
-
-// import Logo from "../../../../public/get.car-logo.png"
-
-
-// const NavBar = () => {
-//     return (
-//         <header>
-//             <div>
-//                 <NavLink to= "/"><img src={Logo} alt="Get.Car" /></NavLink>
-//             </div>
-            
-//             <nav>
-//                 <ul className="flex bg-blue-500 text-white text-center p-4">
-//                     <li>
-//                         <NavLink to="/">Home</NavLink>
-//                     </li>
-//                     <li>
-//                         <NavLink to="/cars">Cars</NavLink>
-//                     </li>
-//                     <li>
-//                         <NavLink to="/about">About Us</NavLink>
-//                     </li>
-//                     <li>
-//                         <NavLink to="/support">Support</NavLink>
-//                     </li>
-//                     <li>
-//                         <NavLink to="/cart"><PiShoppingCartThin /> <span>0</span></NavLink>
-//                     </li>
-                   
-//                 </ul>
-//             </nav>
-//         </header>
-//     );
-// };
-
-// export default NavBar;
-
-
-
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 import { PiShoppingCartThin } from "react-icons/pi";
+
 import Logo from "../../../../public/get.car-logo.png";
+import styles from "./NavBar.module.css";
+
+// react icons
+import { IoMdMenu } from "react-icons/io";
+import { VscChromeClose } from "react-icons/vsc";
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+  const navRef = useRef(null);
+
+  const openMenuHandeling = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const closeMenuHandeling = () => {
+    setOpenMenu(false);
+  };
 
   return (
-    <header className="bg-blue-500 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* Logo */}
-        <NavLink to="/">
-          <img src={Logo} alt="Get.Car" className="h-10" />
-        </NavLink>
+    <header>
+      <div className={`${styles.flex} ${styles.navbar_container}`}>
+        <div className={`${styles.logo}`}>
+          <NavLink to="/" onClick={closeMenuHandeling}>
+            <img src={Logo} alt="Get.Car" />
+          </NavLink>
+        </div>
 
-        {/* Hamburger menu icon for mobile view */}
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="block lg:hidden focus:outline-none"
-        >
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
-          </svg>
-        </button>
-
-        {/* Menu Items */}
-        <nav
-          className={`lg:flex items-center space-x-6 ${isOpen ? 'block' : 'hidden'} lg:block`}
-        >
-          <ul className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-4">
+        <nav className={`${styles.flex} ${styles.menu}`}>
+          <ul
+            ref={navRef}
+            className={`${styles.flex} ${styles.menu_link} ${
+              openMenu ? styles.navResposive : " "
+            }`}
+          >
             <li>
-              <NavLink to="/" className="hover:underline">Home</NavLink>
+              <NavLink
+                onClick={closeMenuHandeling}
+                className={`${({ isActiv }) =>
+                  isActiv ? styles.active : " "}`}
+                to="/"
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/cars" className="hover:underline">Cars</NavLink>
+              <NavLink
+                onClick={closeMenuHandeling}
+                className={`${({ isActiv }) =>
+                  isActiv ? styles.active : " "}`}
+                to="/cars"
+              >
+                Cars
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" className="hover:underline">About Us</NavLink>
+              <NavLink
+                onClick={closeMenuHandeling}
+                className={`${({ isActiv }) =>
+                  isActiv ? styles.active : " "}`}
+                to="/about"
+              >
+                About Us
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/support" className="hover:underline">Support</NavLink>
-            </li>
-            <li>
-              <NavLink to="/cart" className="flex items-center">
-                <PiShoppingCartThin className="mr-1" />
-                <span>0</span>
+              <NavLink
+                onClick={closeMenuHandeling}
+                className={`${({ isActiv }) =>
+                  isActiv ? styles.active : " "}`}
+                to="/support"
+              >
+                Support
               </NavLink>
             </li>
           </ul>
+
+          <div
+            onClick={closeMenuHandeling}
+            className={`${styles.cart_buy} ${styles.flex}`}
+          >
+            <Link to="/cart" className={`${styles.cart} `}>
+              <PiShoppingCartThin /> <span>0</span>
+            </Link>
+            <Link onClick={closeMenuHandeling}>
+              <button className={styles.buyNowBtn}>Buy Now</button>
+            </Link>
+          </div>
         </nav>
+
+        <div onClick={openMenuHandeling} className={styles.menu_btns}>
+          {openMenu ? <VscChromeClose /> : <IoMdMenu />}
+        </div>
       </div>
     </header>
   );
