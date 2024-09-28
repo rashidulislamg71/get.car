8c18916d73b1513912ca69adaa889de16c5dbf1c
rashidulislamg71 <jrashidulislam0103@gmail.com>
2024-09-28 23:25:24 +0600

    how_it_work-design

---
+++ src/Component/HowItWork/HowItWork.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./HowItWork.module.css";
+
+import { MdOutlineSwitchAccount } from "react-icons/md";
+import { IoCarSportOutline } from "react-icons/io5";
+import bid_img from "../../assets/images/how-it-work-img/bid-removebg-preview.png";
+
+import top_arrow from "../../assets/images/how-it-work-img/how_work_arrow_top.png"
+import down_arrow from "../../assets/images/how-it-work-img/how_work_arrow_down.png"
+
+const HowItWork = () => {
+  return (
+    <div className={`${styles.how_it_work_design}`}>
+      <div className={`${styles.how_it_work_container}`}>
+        <div className={`${styles.how_it_work_inner_content} `}>
+          <div className={styles.how_it_work_title}>
+            <h4>Start Now</h4>
+            <h2>How it Works</h2>
+          </div>
+          <div className={`${styles.flex} ${styles.how_it_works}`}>
+            <div className={`${styles.flex} ${styles.single_how_it_work}`}>
+              <div className={` ${styles.flex} ${styles.how_it_work_icon}`}>
+                <span><MdOutlineSwitchAccount /></span>
+              </div>
+              <div className={styles.how_it_work_text}>
+                <h3>Registertion and Account</h3>
+                <p>Search inventory of more than salvage used vehicles. Aliquam sagittis pellentesque turpis egestas tincidunt. Integer mollis leo lectus. .</p>
+              </div>
+            </div>
+            <div className={`${styles.flex} ${styles.single_how_it_work}`}>
+              <div className={` ${styles.flex} ${styles.car_icon} ${styles.how_it_work_icon}`}>
+                <span><IoCarSportOutline /> </span>
+               
+              </div>
+              <div className={styles.how_it_work_text}>
+                <h3>Browse and Select a Car</h3>
+                <p>Search inventory of more than salvage used vehicles. Aliquam sagittis pellentesque turpis egestas tincidunt. Integer mollis leo lectus. .</p>
+              </div>
+            </div>
+            <div className={`${styles.flex} ${styles.single_how_it_work}`}>
+              <div className={` ${styles.flex} ${styles.how_it_work_icon}`}>
+                <span><img src={bid_img} alt="" /></span>
+              </div>
+              <div className={styles.how_it_work_text}>
+                <h3>Place Bids and Monitor</h3>
+                <p>Search inventory of more than salvage used vehicles. Aliquam sagittis pellentesque turpis egestas tincidunt. Integer mollis leo lectus. .</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.top_arrow}>
+          <img src={top_arrow} alt="" />
+          </div>
+          <div className={styles.down_arrow}>
+          <img src={down_arrow} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowItWork;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../../Component/Hero/Hero';
 import HomeAbout from '../../Component/HomeAbout/HomeAbout';
+import HowItWork from '../../Component/HowItWork/HowItWork';
 
 const Home = () => {
     return (
         <div>
-            <Hero />
-            <HomeAbout />
+            {/* <Hero /> */}
+            {/* <HomeAbout /> */}
+            <HowItWork />
         </div>
     );
 };
