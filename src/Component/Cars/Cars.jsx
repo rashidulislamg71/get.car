a2e83f826baabe625905800f19b0b7e9209bf52b
rashidulislamg71 <jrashidulislam0103@gmail.com>
2024-10-07 07:40:48 +0600

    Home cars

--- src/Component/Cars/Cars.jsx
+++ src/Component/Cars/Cars.jsx
@@ -3,37 +3,68 @@
 import { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../GlobalContextAPI/GlobalContext";
 
+import styles from "./Cars.module.css";
+
 const Cars = () => {
-  const { bmw, rolls, selectedProduct, setSelectedProduct } =
+  const { bmw, rolls_royce, toyota, selectedProduct, setSelectedProduct } =
     useContext(ProductContext);
   const [showRolls, setShowRolls] = useState(false);
+  const [showToyota, setShowToyota] = useState(false);
 
   useEffect(() => {
     setSelectedProduct(bmw);
   }, [setSelectedProduct]);
 
-  const handleShowRolls = () => {
-    setShowRolls(true);
-    setSelectedProduct(rolls);
-  };
-
   const handleShowBMW = () => {
     setShowRolls(false);
+    setShowToyota(false);
     setSelectedProduct(bmw);
   };
 
-  return (
-    <>
-      <button onClick={handleShowBMW}>Show BMW</button>
-      <button onClick={handleShowRolls}>Show Rolls</button>
+  const handleShowRollsRoyce = () => {
+    if (showToyota == true) {
+      setShowRolls(false);
+    } else {
+      setShowRolls(true);
+    }
+    setSelectedProduct(rolls_royce);
+  };
 
-      {selectedProduct.map((product) => (
-        <li key={product.id}>
-          <img src={product.img} alt={product.name} />
-          <p>{product.name}</p>
-        </li>
-      ))}
-    </>
+  const handleShowToyota = () => {
+    setShowToyota(true);
+    setSelectedProduct(toyota);
+  };
+
+  return (
+    <div className={styles.cars_container}>
+      <div className={`${styles.flex} ${styles.title_btn}`}>
+        <div className={styles.carsTitle}>
+          <h1>Your Luxurious Cars</h1>
+        </div>
+        <div className={`${styles.flex} ${styles.carsBtn}`}>
+          <button onClick={handleShowBMW}>BMW</button>
+          <button onClick={handleShowRollsRoyce}>Rolls Royce</button>
+          <button onClick={handleShowToyota}>Toyota</button>
+        </div>
+      </div>
+
+      <div className={`${styles.flex} ${styles.cars_content}`}>
+        {selectedProduct.map((product) => (
+          <li key={product.id}>
+            <img src={product.img} alt={product.name} />
+            <div className={styles.cart_details}>
+            <h2>{product.name}</h2>
+            <p className={styles.price}> ${product.price}</p>
+            <div className={`${styles.flex} ${styles.cart_seeDetails_Btn}`}>
+            <button className={styles.addToCartBtn}>Add To Cart</button>
+            <button className={styles.seeDetails}>Details</button>
+            </div>
+            </div>
+            
+          </li>
+        ))}
+      </div>
+    </div>
   );
 };
 
