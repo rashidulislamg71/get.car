032a297c532426de84e76030f47a22bcea65f619
rashidulislamg71 <jrashidulislam0103@gmail.com>
2024-10-12 11:11:57 +0600

    cart item

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import styles from "./Cart.module.css";
 import { ProductContext } from "../GlobalContextAPI/GlobalContext";
+import img from "../../assets/images/cart-car/Rolls-Royce/GTY/car-13.jpg";
+
+import { TiDeleteOutline } from "react-icons/ti";
 
 const Cart = () => {
   const { cart } = useContext(ProductContext);
@@ -8,16 +11,84 @@ const Cart = () => {
   return (
     <div className={`${styles.cart_wrapper}`}>
       <div className={`${styles.cart_container}`}>
-        <div className={`${styles.flex} ${styles.cart_inner_content}`}>
-          <div className={`${styles.cart_items}`}></div>
-          <div className={`${styles.cart_price_deatils}`}></div>
+        <div className={`${styles.cart_inner_content}`}>
+          <div>
+            <h2>Your Cars</h2>
+            <div className={`${styles.flex} ${styles.cart_title}`}>
+              <b>CAR DETAILS</b>
+              <b>QUNTITY</b>
+              <b>PRICE</b>
+              <b>TOTAL</b>
+            </div>
+          </div>
+
+          <div
+            className={`${styles.flex} ${styles.cart_items_and_order_summary}`}
+          >
+            <div className={`${styles.flex}  ${styles.cart_items}`}>
+              <div className={styles.flex}>
+                <div>
+                  <img src={img} alt="" />
+                </div>
+                <div>
+                  <b>
+                    Band: <span>BMW</span>
+                  </b>
+                  <br />
+                  <b>
+                    Model: <span>M3</span>
+                  </b>
+                  <br />
+                  <b>
+                    Color: <span>Blue</span>
+                  </b>
+                </div>
+              </div>
+              <div className={`${styles.flex} ${styles.quntity}`}>
+                <p>1</p>
+                <div className={`${styles.flex} ${styles.quntity_btn}`}>
+                  <button>+</button>
+                  <button>-</button>
+                </div>
+              </div>
+              <div className={`${styles.flex} ${styles.price}`}>
+                <p>$23903.00</p>
+              </div>
+              <div className={`${styles.flex} ${styles.total_price}`}>
+                <p>$23903.00</p>
+              </div>
+              <div className={`${styles.flex} ${styles.delete_cart}`}>
+                <button>
+                  <TiDeleteOutline />
+                </button>
+              </div>
+            </div>
+
+            <div className={`${styles.oder_summary}`}>
+              <h2>Order Summary</h2>
+              <div className={` ${styles.flex} ${styles.summary_deatils}`}>
+                <div className={`${styles.summary_deatils_title}`}>
+                  <p>CAR ITEMS</p>
+                  <p>PRICE</p>
+                  <p>DELIVERY CHARGE</p>
+                  <p>TOTAL COST</p>
+                </div>
+                <div className={`${styles.summary_deatils_data}`}>
+                  <p>1</p>
+                  <p>$230329.00</p>
+                  <p>$00.00</p>
+                  <p>$230329.00</p>
+                </div>
+              </div>
+              <button>ORDER NOW</button>
+            </div>
+          </div>
         </div>
 
         <div className={` ${styles.flex} ${styles.cart_adderss_and_payment}`}>
           <div className={`${styles.address}`}>
             <h2>Your Address</h2>
             <form className={` ${styles.flex} ${styles.address_form}`}>
-
               <div className={`${styles.flex} ${styles.address_row_1}`}>
                 <div>
                   <label>Full Name</label> <br />
@@ -50,7 +121,11 @@ const Cart = () => {
               </div>
               <div className={styles.address_row_2}>
                 <label>Massage</label> <br />
-                <textarea rows="4" cols="20" placeholder="Say your opinion....."/>
+                <textarea
+                  rows="4"
+                  cols="20"
+                  placeholder="Say your opinion....."
+                />
               </div>
               <button>Submit</button>
             </form>
@@ -59,16 +134,24 @@ const Cart = () => {
           <div className={`${styles.payment_method}`}>
             <h2>Payment Now</h2>
             <div className={`${styles.payment_banks}`}>
-                <a href=""><h4>Islami Bank</h4></a>
-                <a href=""><h4>Sonali Bank</h4></a>
-                <a href=""><h4>Janata Bank</h4></a>
-                <a href=""><h4>HSBC Bank</h4></a>
-                <input  type="checkbox" /> <span className={styles.cash_on_delevery}>Cash On Delevery</span>
-                <br />
-                <div className={`${styles.flex} ${styles.order_nowBtn}`}>
-
+              <a href="">
+                <h4>Islami Bank</h4>
+              </a>
+              <a href="">
+                <h4>Sonali Bank</h4>
+              </a>
+              <a href="">
+                <h4>Janata Bank</h4>
+              </a>
+              <a href="">
+                <h4>HSBC Bank</h4>
+              </a>
+              <input type="checkbox" />{" "}
+              <span className={styles.cash_on_delevery}>Cash On Delevery</span>
+              <br />
+              <div className={`${styles.flex} ${styles.order_nowBtn}`}>
                 <button>Order Now</button>
-                </div>
+              </div>
             </div>
           </div>
         </div>
